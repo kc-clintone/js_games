1adcc8f393fb700468f0bab28bafdb854b5c092c
kc-clintone <kayseeclintone@gmail.com>
2024-03-21 18:44:45 +0000

    starting build

---
+++ main.js
@@ -0,0 +1,15 @@
+let blockSize = 15;
+let width, height = 20;
+let gameBoard, context;
+
+let x, y = blockSize * 5;
+let snakeSpeedX, snakeSpeedY = 0;
+let snakeBody = []
+
+let fx, fy;
+
+let gameOver = false;
+
+window.onload = function(){
+
+}
